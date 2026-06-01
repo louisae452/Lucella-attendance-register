deb6f686dc69b393b24045325d5f639eac832ba8
louisae452 <louisae452@msn.com>
2026-06-01 19:52:23 +0100

    Add all fields to students_list view

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
 from .views import HomeView
+#from .views import StudentsList
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('student/', views.students_list, name='students'),
-]
+   # path('student/', StudentsList.as_view(), name='students'),
+]#
--- attendance/views.py
+++ attendance/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Student
@@ -16,10 +17,18 @@ class HomeView(TemplateView):
 # Check person is logged in and has required permission(admissions_officer group)
 
 
+
+#lass StudentsList(generic.ListView):
+#    queryset = Student.objects.all()
+#    template_name = "attendance/students_list.html"
+
+
 @login_required
 # @permission_required("student.add_student", raise_exception=True)
 
 def students_list(request):
-    students = Student.objects.all().values()
+    students = Student.objects.all()
+    
     return render(request, "attendance/students_list.html", {"students":students})
+    #return object_list(request, template_name="attendance.students_list,html", queryset=students)
     
