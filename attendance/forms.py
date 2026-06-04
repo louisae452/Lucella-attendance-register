11f94756bd3cecd977f97ba5895fda98c3e687cf
louisae452 <louisae452@msn.com>
2026-06-04 19:52:07 +0100

    Style student and parent registration pages

--- attendance/forms.py
+++ attendance/forms.py
@@ -8,17 +8,18 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ('student_name', 'student_surname', 'student_code', 'parent_name', 'date_of_birth', 'sex', 'group', 'music_option', )
-                  
-class UserNameForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('username',)
- 
+        help_texts = {
+            'student_code': 'The student code is the last two digits of the year of birth, month of birth, first two letters of surname followed by first two letters of name in capitals.',
+        }          
+
                   
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password',)
+        help_texts = {
+            'username': 'The user name is the name followed by surname wihtout spaces',
+        }
         
 class ParentForm(forms.ModelForm):
     class Meta:
